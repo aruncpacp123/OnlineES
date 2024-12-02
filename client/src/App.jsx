dd34d712ca7400fabc4885428a41675e44a1fbf1
ARUN C P <aruncpacp10@gmail.com>
2024-12-02 21:32:04 +0530

    Update App.jsx  with private routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,104 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import SignIn from './login/pages/signIn';
-import SignUp from './login/pages/signup';
-import Institution from './login/pages/institutionSignUp';
-import { UserProvider } from './login/utils/contexts/userContext';
-import Home from './admin/pages/home';
-import Courses from './admin/components/courses';
-import StudentHome from './student/pages/studentHome';
-import TeacherHome from './teacher/pages/teacherHome';
-import ExamForm from './teacher/components/ExamForm';
-import QuizForm from './teacher/components/quizForm';
-import SubjectiveForm from './teacher/components/SubjectiveForm';
-import Quiz from './student/components/Quiz';
-import Subjective from './student/components/Subjective';
-import { Profile } from './teacher/components/profile';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./login/pages/signIn";
+import SignUp from "./login/pages/signup";
+import Institution from "./login/pages/institutionSignUp";
+import Home from "./admin/pages/home";
+import StudentHome from "./student/pages/studentHome";
+import TeacherHome from "./teacher/pages/teacherHome";
+import ExamForm from "./teacher/components/ExamForm";
+import QuizForm from "./teacher/components/quizForm";
+import SubjectiveForm from "./teacher/components/SubjectiveForm";
+import Quiz from "./student/components/Quiz";
+import Subjective from "./student/components/Subjective";
+import { Profile } from "./teacher/components/profile";
+import PrivateRoute from "./PrivateRoute"; // Import the modified PrivateRoute
+
 function App() {
-  return(
-    <UserProvider>{/*Also include Priavte routing */}
-        <Router>
-            <Routes>
-              {/* Login Paths */}
-                <Route path="/" element={<SignIn/>} />
-                <Route path="/login" element={<SignIn />} />
-                <Route path="/register" element={<SignUp />} />
-                <Route path="/Instregister" element={<Institution />} />
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<SignIn />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/Instregister" element={<Institution />} />
 
-              {/* Admin Paths */}
-
-              <Route path='/admin' element={<Home />} ></Route>``
-
-
-              {/* Teacher Paths */}
-              <Route path='/teacher' element={<TeacherHome />} />
-              <Route path='/teacher/exam' element={<ExamForm/>} />
-              <Route path='/teacher/exam/step1' element={<QuizForm />} />
-              <Route path='/teacher/exam/step2' element={<SubjectiveForm />} />
-              <Route path='/profile' element={<Profile />} />
-
-
-              {/* Student Paths */}
-              <Route path='/student' element={<StudentHome />} />
-              <Route path='/student/exam/quiz' element={<Quiz />} />
-              <Route path='/student/exam/subjective' element={<Subjective />} />
-
-
-            </Routes>
-        </Router>
-    </UserProvider>
-
-  )
+        {/* Private Routes */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/teacher"
+          element={
+            <PrivateRoute>
+              <TeacherHome />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/teacher/exam"
+          element={
+            <PrivateRoute>
+              <ExamForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/teacher/exam/step1"
+          element={
+            <PrivateRoute>
+              <QuizForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/teacher/exam/step2"
+          element={
+            <PrivateRoute>
+              <SubjectiveForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/student"
+          element={
+            <PrivateRoute>
+              <StudentHome />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/student/exam/quiz"
+          element={
+            <PrivateRoute>
+              <Quiz />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/student/exam/subjective"
+          element={
+            <PrivateRoute>
+              <Subjective />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
