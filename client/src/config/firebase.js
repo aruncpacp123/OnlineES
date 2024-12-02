a03143bf58bff81da55b5d34337799a5a9666ca9
ARUN C P <aruncpacp10@gmail.com>
2024-12-02 22:45:45 +0530

    key

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -8,6 +8,8 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//Store Api Key in .env file and push it into github for security
 const firebaseConfig = {
   apiKey: "AIzaSyDhIBNvZ_oD9XRMTV9bs1lCeescu0OQowk",
   authDomain: "onlineexaminationsystem-d2293.firebaseapp.com",
