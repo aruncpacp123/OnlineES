a9ea766f922d5f0a7452c82a2187ab3871f05f42
aruncpacp123 <aruncpacp10@gmail.com>
2024-12-04 18:37:33 +0530

    Changed for linux environment

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 5000 ;
 
 const db=mysql.createConnection({
     host:DB_HOST,
